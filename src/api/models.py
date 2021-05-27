8995b698f07a389257185f1a8b4a5fe65d89196c
Andreagar28 <agarchig28@gmail.com>
2021-05-27 15:08:30 +0000

    base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,80 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    name = db.Column(db.String)
+    mail = db.Column(db.String)
+    password = db.Column(db.String) 
+    age = db.Column(db.Integer, nullable=True)
+    localization = db.Column(db.String, nullable=True)
+    bio = db.Column(db.String, nullable=True)
+    
+    # #followers = db.relationship("Followers",
+    # secondary=association_table)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Trip(db.Model):
+    __tablename__ = 'trip'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    country = db.Column(db.String)
+    cities = db.Column(db.String) 
+    activities = db.Column(db.String)
+    done = db.Column(db.Boolean, default=False) 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Share_Trip(db.Model):
+    __tablename__ = 'share_trip'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    trip_id = db.Column(db.Integer, db.ForeignKey("trip.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id")) 
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    title = db.Column(db.String)
+    media = db.Column(db.String)
+    text = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship(User)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    text = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    user = db.relationship(User)
+    post = db.relationship(Post)
+
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey("chat.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    message = db.Column(db.String)
+
+class Chat(db.Model):
+    __tablename__ = 'chat'
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey("chat.id"))
+    name = db.Column(db.String)
+
+
+
+   
+
+    def to_dict(self):
+        return {}
+
+
+
