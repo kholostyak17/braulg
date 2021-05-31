94c5f8d01c476bfd3bdd54a0a431d1418198e095
kholostyak17 <kholostyak17@gmail.com>
2021-05-31 12:42:44 +0000

    component button created

---
+++ src/front/js/component/button.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+const Button = props => {
+	let style = "button ".concat(props.className, " ", props.size, " ", props.color);
+	return (
+		<button className={style} type="button">
+			{props.text}
+		</button>
+	);
+};
+
+export default Button;
+
+Button.propTypes = {
+	className: PropTypes.string,
+	size: PropTypes.string,
+	color: PropTypes.string,
+	text: PropTypes.string
+};
