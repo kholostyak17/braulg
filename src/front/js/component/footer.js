b61e600c606bc35fb9935457b9ad6ae74bbe9353
kholostyak17 <kholostyak17@gmail.com>
2021-05-31 16:04:01 +0000

    footer done

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/footer.scss";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-1 text-center">
+		<span>
+			Made with <i className="fa fa-heart text-danger" /> by Andrea, Sergio &amp; Iván
+		</span>
+		<div>
+			<Link to="/aboutus">
+				<a href="#" className="link-footer">
+					Acerca de nosotros
+				</a>
+			</Link>
+			<Link to="/contact">
+				<a href="#" className="link-footer">
+					Contacta
+				</a>
+			</Link>
+			<Link to="/faq">
+				<a href="#" className="link-footer">
+					Preguntas frecuentes
+				</a>
+			</Link>
+		</div>
 	</footer>
 );
