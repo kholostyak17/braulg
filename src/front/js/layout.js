2541b84fefb2064f5cdfcc60154c49ae7094d766
kholostyak17 <kholostyak17@gmail.com>
2021-06-01 14:12:28 +0000

    upgraded navbar bootstrap vainilla -> bootstrap react

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -8,7 +8,7 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { MyNavbar } from "./component/my-navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -21,7 +21,7 @@ const Layout = () => {
 		<div className="d-flex flex-column h-100">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
+					<MyNavbar />
 					<Switch>
 						<Route exact path="/">
 							<Home />
