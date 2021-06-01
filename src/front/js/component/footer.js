2f956e79164d2829d7676e15a4aeacc4eb0c3609
kholostyak17 <kholostyak17@gmail.com>
2021-06-01 16:23:17 +0000

    responsive footer -> done finally

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import "../../styles/footer.scss";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-1 text-center d-flex flex-wrap">
-		<div className="ms-3 me-auto">
-			Made with <i className="fa fa-heart text-danger" /> by Andrea, Sergio &amp; Iván
-		</div>
+	<footer className="footer mt-auto py-1 px-3">
+		<Link to="#">
+			<div className="text-footer col-12 col-sm-12 col-md-5">
+				Made with <i className="fa fa-heart text-danger" /> by Andrea, Sergio &amp; Iván
+			</div>
+		</Link>
 		<Link to="/aboutus">
-			<div className="link-footer">Acerca de nosotros</div>
+			<div className="link-footer col-12 col-sm-3 col-md-2">Acerca de nosotros</div>
 		</Link>
 		<Link to="/contact">
-			<div className="link-footer">Contacta</div>
+			<div className="link-footer col-12 col-sm-4 col-md-2 ">Contacta</div>
 		</Link>
 		<Link to="/faq">
-			<div className="link-footer">Preguntas frecuentes</div>
+			<div className="link-footer col-12 col-sm-3 col-md-2 ">Preguntas frecuentes</div>
 		</Link>
 	</footer>
 );
