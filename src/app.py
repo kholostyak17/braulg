4bbd06d1079f7d1f85bd7aa35e7519497f6720d1
kholostyak17 <kholostyak17@gmail.com>
2021-06-17 14:31:46 +0000

    app.py database configuration updated

--- src/app.py
+++ src/app.py
@@ -30,6 +30,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
