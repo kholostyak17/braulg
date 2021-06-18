101ccf60675726eca0f07ed1fc4d0baaad6a48f3
kholostyak17 <kholostyak17@gmail.com>
2021-06-18 14:37:05 +0000

    seed data

--- src/api/seed_data.py
+++ src/api/seed_data.py
@@ -8,7 +8,7 @@ data = {
             "language": "english",
             "age": "30",
             "localization": "Colonia (Alemania)",
-            "bio": "Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.",
+            "bio": "¡Hola¡ Me llamo Persefone Viehman y estoy ilusionado de vivir con vosotros una aventura parael mes de enero! Muchas gracias por permitirme ser el promotor este viaje. Tengo veinte años  y soy un estudiante de la universidad. Nací en el estado de Pennsylvania, pero ahora vivo en Colonia (Alemania) con mi familia. Me encantaría viajar a Andorra pea conocer su increíble paisaje..",
             "profile_picture":"https://img.icons8.com/bubbles/452/flag-person-female.png"
         },
         {
@@ -19,7 +19,7 @@ data = {
             "language": "spanish",
             "age": "25",
             "localization": "Paris (Francia)",
-            "bio": "Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.",
+            "bio": "Hola mi nombre es Jacques, soy de Tudela, un pequeño pueblo de Navarra, pero llevo viviendo en París durante más de 20 años debido a mi gran pasión, la arquitectura! Me entusiasma la idea de viajar a Costa Rica por su gran biodiversidad, quién se apunta?.",
             "profile_picture": "https://alquilerautocaravanaslanzarote.com/wp-content/uploads/icons8-female-user-200.png"
         },
         {
@@ -30,7 +30,7 @@ data = {
             "language": "spanish",
             "age": "26",
             "localization": "Madrid (España)",
-            "bio": "Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.Aenean laoreet malesuada purus vitae imperdiet. Praesent id ligula quis leo ornare venenatis id sit amet erat.",
+            "bio": "Me llamo Manuela, soy de Colombia, pero estoy estudiando en Madrid, y me encantaría conocer gente para viajar a Austria ya que me encantaría tener la oportunidad de conocer La Abadía de Melk, os espero!!! ",
             "profile_picture": "https://img.icons8.com/bubbles/2x/see-male-account.png"
         },
         {
@@ -44,7 +44,6 @@ data = {
             "bio": "Me gusta mucho la pesca, me he criado siempre cerca del rio y soy muy sociable.",
             "profile_picture": "https://img.icons8.com/bubbles/2x/user-male.png"
         },
-
     ],
     "Post": [
         {
@@ -52,21 +51,21 @@ data = {
             "traveler_id": 1,
             "title": "Viaje a Jerusalen",
             "media": "https://www.vaticannews.va/content/dam/vaticannews/images-multimedia/medio-oriente/Gerusalemme%20panorama%20con%20Moschea%20della%20Rocca.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.1000.563.jpeg",
-            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "text": "Jerusalén, la capital de Israel, es una ciudad santa para las tres grandes religiones monoteístas: El judaísmo, el cristianismo y Jerusalem; se encuentra en las montañas de Judea entre el Mar Mediterráneo y el Mar Muerto Islam. A pesar de todos los problemas que rodean la ciudad, presenta un conjunto armónico, usted puede sentir como el peso de los alrededores espirituales desciende sobre sus hombros tan pronto como entras en la ciudad. La ciudad vieja y sus murallas forman un patrimonio de la humanidad. Esta antigua ciudad es el hogar de los lugares sagrados para Judios, cristianos y musulmanes, y está lleno de lugares de importancia religiosa masiva y un acercamiento a la vida bulliciosa. Los principales lugares de interés dentro de la ciudad vieja son tres: el Muro de las Lamentaciones, la Iglesia de la Santa Supulchre y el área del Monte del Templo, que incluye la Cúpula de la mezquita de la roca y Al Aqsa. La Vía Dolorosa es la calle más popular dentro de la ciudad vieja, sobre todo entre los peregrinos cristianos ya que este era el camino que Jesucristo caminó sobre el camino a su crucifixión. Hay 14 estaciones de la cruz en el camino a la estación del Vía Crucis. La ubicación final y más importante en el cristianismo se encuentra en el Centro de la Iglesia del Santo Sepulcro. La Jerusalén moderna, donde se encuentran la mayoría de los hoteles y restaurantes, está construido alrededor de la Ciudad Vieja. Jerusalén es el más sagrado de Ciudades Santas y ofrece mucho para ver y explorar. La ciudad es una fascinante mezcla de tradición, religión y vida moderna. Se las arregla para que se sienta totalmente exótico y misterioso, y al mismo tiempo que familiar; todo a la vez"..",
         },
         {
             "id": 1,
             "traveler_id": 2,
             "title": "Viaje a Bogotá",
             "media": "https://www.avianca.com/content/dam/avianca_new/destinos/bog/co_bog_porquevisitar2.jpg?lazy=true",
-            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "text": "Si vas a viajar a Bogotá , muy pronto te darás cuenta de la belleza de todos sus rincones. Sin embargo, te recomendamos encarecidamente que optes por algunas recomendaciones prácticas, especialmente en su capital. ¿Alguna vez has oído eso de que en las ciudades grandes corres más peligro que en las aldeas pequeñas? Estamos completamente de acuerdo. Por eso, con estos sencillos consejos lograrás el objetivo. Llevar un recuerdo maravilloso de esta bella tierra.",
         },
         {
             "id": 2,
             "traveler_id": 0,
             "title": "Viaje a Hong Kong",
             "media": "https://img.static-kl.com/images/media/4EA8CE28-6BF4-4503-88618BAEF81EE29C?aspect_ratio=1:1&min_width=456",
-            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "text": "Hong Kong, conocida como la Perla de Asia o la Nueva York asiática, es una ciudad vibrante, cosmopolita, que combina modernidad y tradición, y que no deja indiferente a sus visitantes. Hoy desde IATI, expertos en seguros de viajeros, queremos daros una serie de consejos para viajar a Hong Kong, un listado de imprescindibles que deberéis tener en cuenta a la hora de organizar una visita a esta urbe china”.
         }
     ],
     "Trip": [
