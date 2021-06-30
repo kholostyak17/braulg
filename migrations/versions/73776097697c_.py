30abbe9b05a6178d5fc9989d7d3d283d4e28787b
kholostyak17 <kholostyak17@gmail.com>
2021-06-30 22:24:36 +0000

    migrations done

--- migrations/versions/73776097697c_.py
+++ migrations/versions/73776097697c_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5be076540408
+Revision ID: 73776097697c
 Revises: 
-Create Date: 2021-06-30 15:58:28.222076
+Create Date: 2021-06-30 22:23:13.773052
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5be076540408'
+revision = '73776097697c'
 down_revision = None
 branch_labels = None
 depends_on = None
